8badb8b2c0582e389536925faa34a14e3d35e455
oruban <settlers.net@gmail.com>
2017-10-13 22:04:33 +0300

    add swagger tools

--- src/Experimentarium.AspNetCore.WebApi/Startup.cs
+++ src/Experimentarium.AspNetCore.WebApi/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Experimentarium.AspNetCore.WebApi
 {
@@ -29,6 +31,21 @@ namespace Experimentarium.AspNetCore.WebApi
             services.Configure<MyOptions>(Configuration.GetSection("MyOptions"));
 
             services.AddMvc();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { 
+                    Title = "Experimentarium API V1", 
+                    Version = "v1", 
+                    Description = "Different experiments with ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact(){ Url = "https://github.com/ORuban/aspnet-core-experimentarium" }
+                    });
+
+                c.IgnoreObsoleteActions();
+                c.IgnoreObsoleteProperties();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +55,17 @@ namespace Experimentarium.AspNetCore.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            // https://docs.microsoft.com/en-us/aspnet/core/tutorials/web-api-help-pages-using-swagger?tabs=visual-studio
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            // By default available by /swagger endpoint
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Experimentarium API V1");
+            });
 
             app.UseMvc();
         }
